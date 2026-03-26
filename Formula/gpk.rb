9353a87b82258ca1039b29421300f9542d2d5da8
neur0map <90535780+neur0map@users.noreply.github.com>
2026-03-26 19:16:13 +0000

    gpk 0.3.11

--- Formula/gpk.rb
+++ Formula/gpk.rb
@@ -1,32 +1,31 @@
 class Gpk < Formula
   desc "TUI dashboard that unifies 34 package managers into one searchable view"
   homepage "https://github.com/neur0map/glazepkg"
-  version "0.3.6"
+  version "0.3.11"
   license "GPL-3.0-or-later"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/neur0map/glazepkg/releases/download/v0.3.6/gpk-darwin-arm64"
-      sha256 "0882160721baf269e8ed6612f943470f730a99087d6c0015904a104edb0221a4"
+      url "https://github.com/neur0map/glazepkg/releases/download/v0.3.11/gpk-darwin-arm64"
+      sha256 "7082cab5a4e55d03a42fb94b31523978b877ec9e55355d9846665629c5305de1"
     else
-      url "https://github.com/neur0map/glazepkg/releases/download/v0.3.6/gpk-darwin-amd64"
-      sha256 "9839fedc32dd71af6a59b02abf91fc784fbdafd3e61f1f24dba485cf842a54c0"
+      url "https://github.com/neur0map/glazepkg/releases/download/v0.3.11/gpk-darwin-amd64"
+      sha256 "dc73d85df1d3c73a9785b776e287f7160ae14f2710c7272ee8884cc0734b25ef"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/neur0map/glazepkg/releases/download/v0.3.6/gpk-linux-arm64"
-      sha256 "5514945dbf0b93eef00d7fa66732ad3d70c8164eabf48d1941ce01ce8fd85523"
+      url "https://github.com/neur0map/glazepkg/releases/download/v0.3.11/gpk-linux-arm64"
+      sha256 "b199d791f4aec0713e1a350c8f18023267ccfad0be83dfad7a138ef5b944f08d"
     else
-      url "https://github.com/neur0map/glazepkg/releases/download/v0.3.6/gpk-linux-amd64"
-      sha256 "3fbc82420a3e8def80cb29e49e46cd4145a37f4d33c0a56117c2ae8e9e009801"
+      url "https://github.com/neur0map/glazepkg/releases/download/v0.3.11/gpk-linux-amd64"
+      sha256 "cac0211a454708c8d6b17a5818ca16ee0c22f461b168239cd94377313fe21416"
     end
   end
 
   def install
-    binary = Dir.glob("gpk-*").first
-    bin.install binary => "gpk"
+    bin.install Dir["gpk-*"].first => "gpk"
   end
 
   test do
